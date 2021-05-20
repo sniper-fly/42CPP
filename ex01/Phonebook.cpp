687c1d8267afc2d178514a79f427a27fc1949ed0
rnakai <rnakai@student.42tokyo.jp>
2021-05-20 17:53:42 +0900

    誕生日設定メソッドの実装

--- ex01/Phonebook.cpp
+++ ex01/Phonebook.cpp
@@ -3,7 +3,18 @@
 #include <iostream>
 #include <regex>
 
-Phonebook::Phonebook(void) { _is_empty = true; }
+Phonebook::Phonebook(void)
+{
+	_is_empty = true;
+	_birthday_year = 0;
+	_birthday_month = 0;
+	_birthday_date = 0;
+	for (int i = 0; i < TOTAL_DATA; i++) {
+		string_data[i] = "";
+	}
+	_phone_number = "";
+	_email_address = "";
+}
 
 Phonebook::~Phonebook(void) { return ;}
 
@@ -36,15 +47,24 @@ int		Phonebook::setEmailAddress(std::string email_address)
 	return (true);
 }
 
+static int is_digitstr(std::string number)
+{
+	if (number[0] == '\0')
+		return (false);
+	for (int i = 0; number[i]; i++) {
+		if (!isdigit(number[i]))
+			return (false);
+	}
+	return (true);
+}
+
 int		Phonebook::setPhoneNumber(std::string phone_number)
 {
 	if (phone_number[0] == '\0')
 		;
 	else {
-		for (int i = 0; phone_number[i]; i++) {
-			if (!isdigit(phone_number[i]))
-				return (false);
-		}
+		if (!is_digitstr(phone_number))
+			return (false);
 	}
 	this->_phone_number = phone_number;
 	return (true);
@@ -54,18 +74,52 @@ int			Phonebook::setBirthdayYear(std::string year)
 {
 	int y;
 
+	if (year[0] == '\0')
+		return (true);
+	if (!is_digitstr(year))
+		return (false);
 	y = std::stoi(year);
 	this->_birthday_year = y;
+	return (true);
 }
 
 int			Phonebook::setBirthdayMonth(std::string month)
 {
 	int m;
+
+	if (month[0] == '\0')
+		return (true);
+	if (!is_digitstr(month))
+		return (false);
 	m = std::stoi(month);
+	if (!(1 <= m && m <= 12)) {
+		return (false);
+	}
+	this->_birthday_month = m;
+	return (true);
 }
 
 int			Phonebook::setBirthdayDate(std::string date)
 {
 	int d;
+
+	if (date[0] == '\0')
+		return (true);
+	if (!is_digitstr(date))
+		return (false);
 	d = std::stoi(date);
+	if (_birthday_month == 1 || _birthday_month == 3 || _birthday_month == 5 ||
+		_birthday_month == 7 || _birthday_month == 8 || _birthday_month == 10 ||
+		_birthday_month == 12)
+	{
+		if (!(1 <= d && d <= 31)) {
+			return (false);
+		}
+	}
+	else {
+		if (!(1 <= d && d <= 30))
+			return (false);
+	}
+	_birthday_date = d;
+	return (true);
 }
