43b6bbdcf654c38e54bff203b889480a8ee533d5
rnakai <rnakai@student.42tokyo.jp>
2021-05-20 15:51:26 +0900

    setPhoneNumberとsetEmailAddressの実装

--- ex01/Phonebook.cpp
+++ ex01/Phonebook.cpp
@@ -1,50 +1,55 @@
 
 #include "Phonebook.hpp"
 #include <iostream>
+#include <regex>
 
-Phonebook::Phonebook(void)
+Phonebook::Phonebook(void) { _is_empty = true; }
+
+Phonebook::~Phonebook(void) { return ;}
+
+bool			Phonebook::isEmpty(void) { return _is_empty;}
+
+int				Phonebook::getIndex(void) { return _index; }
+
+std::string		Phonebook::getStrData(int i) { return string_data[i]; }
+std::string		Phonebook::getPhoneNumber(void) { return _phone_number; }
+std::string		Phonebook::getEmailAddress(void) { return _email_address; }
+int				Phonebook::getBirthdayYear(void) { return _birthday_year; }
+int				Phonebook::getBirthdayMonth(void) { return _birthday_month; }
+int				Phonebook::getBirthdayDate(void) { return _birthday_date; }
+
+void	Phonebook::setStrData(int i, std::string data) { this->string_data[i] = data; }
+
+static int is_email_valid(std::string email_address)
 {
-	_is_empty = true;
+	const std::regex pattern("(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+");
+	return (std::regex_match(email_address, pattern));
 }
 
-Phonebook::~Phonebook(void)
+int		Phonebook::setEmailAddress(std::string email_address)
 {
-	return ;
+	if (email_address[0] == '\0')
+		;
+	else if (!is_email_valid(email_address))
+		return (false);
+	this->_email_address = email_address;
+	return (true);
 }
 
-int				Phonebook::getIndex(void)
-{
-	return _index;
-}
+static int ft_isnum(char ch) { return ('0' <= ch && ch <= '9'); }
 
-bool			Phonebook::isEmpty(void)
+int		Phonebook::setPhoneNumber(std::string phone_number)
 {
-	return _is_empty;
-}
-
-std::string		Phonebook::getStrData(int i)
-{
-	return string_data[i];
-}
-
-int				Phonebook::getBirthdayYear(void)
-{
-	return _birthday_year;
-}
-
-int				Phonebook::getBirthdayMonth(void)
-{
-	return _birthday_month;
-}
-
-int				Phonebook::getBirthdayDate(void)
-{
-	return _birthday_date;
-}
-
-void		Phonebook::setStrData(int i, std::string data)
-{
-	this->string_data[i] = data;
+	if (phone_number[0] == '\0')
+		;
+	else {
+		for (int i = 0; phone_number[i]; i++) {
+			if (!ft_isnum(phone_number[i]))
+				return (false);
+		}
+	}
+	this->_phone_number = phone_number;
+	return (true);
 }
 
 // int			Phonebook::setBirthdayYear(int year)
--- ex01/Phonebook.hpp
+++ ex01/Phonebook.hpp
@@ -34,54 +34,31 @@ class		Phonebook
 	private:
 		int			_index;
 		bool		_is_empty;
-		// std::string	_first_name;
-		// std::string	_last_name;
-		// std::string	_nickname;
-		// std::string	_login;
-		// std::string	_postal_address;
-		// std::string	_email_address;
-		// int			_phone_number; //TODO:string型に変更するべき
 		int			_birthday_year;
 		int			_birthday_month;
 		int			_birthday_date;
-		// std::string	_favorite_meal;
-		// std::string	_underwear_color;
-		// std::string _darkest_secret;
 		std::string string_data[TOTAL_DATA];
+		std::string	_phone_number;
+		std::string	_email_address;
 
 	public:
-		std::string		getStrData(int i);
-		int				getIndex(void);
 		bool			isEmpty(void);
-		// std::string		getFirstName(void);
-		// std::string		getLastName(void);
-		// std::string		getNickName(void);
-		// std::string		getLogin(void);
-		// std::string		getPostalAddress(void);
-		// std::string		getEmailAddress(void);
-		int				getPhoneNumber(void);
+		int				getIndex(void);
+
+		std::string		getStrData(int i);
+		std::string		getPhoneNumber(void);
+		std::string		getEmailAddress(void);
 		int				getBirthdayYear(void);
 		int				getBirthdayMonth(void);
 		int				getBirthdayDate(void);
-		// std::string		getFavoriteMeal(void);
-		// std::string		getUnderwearColor(void);
-		// std::string		getDarkestSecret(void);
 
 		void		setStrData(int i, std::string data);
-		// void		setFirstName(std::string first_name);
-		// void		setLastName(std::string last_name);
-		// void		setNickName(std::string nick_name);
-		// void		setLogin(std::string login);
-		// void		setPostalAddress(std::string postal_address);
-		// void		setProperty(std::string &to_set, std::string property);
-		// int			setEmailAddress(std::string email_address);
-		// int			setPhoneNumber(int phone_number);
+		int			setEmailAddress(std::string email_address);
+		int			setPhoneNumber(std::string phone_number);
 		int			setBirthdayYear(int year);
 		int			setBirthdayMonth(int month);
 		int			setBirthdayDate(int date);
-		// void		setFavoriteMeal(std::string meal);
-		// void		setUnderwearColor(std::string color);
-		// void		setDarkestSecret(std::string darkest_secret);
+
 		Phonebook(void);
 		~Phonebook(void);
 };
