f668765311eb23e059688a411143836793dc4572
rnakai <rnakai@student.42tokyo.jp>
2021-05-28 11:33:23 +0900

    search strが空を渡されたときの例外処理を追加

--- cpp01/ex07/main.cpp
+++ cpp01/ex07/main.cpp
@@ -14,6 +14,10 @@ int		main(int argc, char **argv)
 		std::cout << "Error: Invalid number of argument" << std::endl;
 		return (1);
 	}
+	if (argv[REPLACED][0] == '\0') {
+		std::cout << "Error: Empty value to search" << std::endl;
+		return (1);
+	}
 	Replace::ft_sed(argv[FILE_NAME], argv[REPLACED], argv[DEST]);
 	return (0);
 }
