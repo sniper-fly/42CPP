6d6c2a5b5605c908703418e6abf17f71bf25ad60
rnakai <rnakai@student.42tokyo.jp>
2021-06-19 13:34:31 +0900

    get_input_file関数等を削除

--- cpp01/ex07/Replace.cpp
+++ cpp01/ex07/Replace.cpp
@@ -34,27 +34,6 @@ void Replace::ft_sed(std::string file_name, std::string replaced, std::string de
 	ofs.close();
 }
 
-std::ofstream Replace::get_outputstream(std::string file_name)
-{
-	file_name += ".replace";
-	std::ofstream ofs(file_name);
-	if (!ofs) {
-		std::cout << "Error: Failed to create the file: " << file_name << std::endl;
-		exit(2);
-	}
-	return (ofs);
-}
-
-std::ifstream Replace::get_inputstream(std::string file_name)
-{
-	std::ifstream ifs(file_name);
-	if (!ifs) {
-		std::cout << "Error: Failed to open the file: " << file_name << std::endl;
-		exit (3);
-	}
-	return (ifs);
-}
-
 std::string Replace::replace_str(std::string buf, std::string replaced, std::string dest)
 {
 	std::string::size_type pos;
--- cpp01/ex07/Replace.hpp
+++ cpp01/ex07/Replace.hpp
@@ -9,8 +9,6 @@ class Replace
 public:
 	static void ft_sed(std::string file_name, std::string replaced, std::string dest);
 private:
-	static std::ofstream get_outputstream(std::string file_name);
-	static std::ifstream get_inputstream(std::string file_name);
 	static std::string replace_str(std::string buf, std::string replaced, std::string dest);
 };
 
