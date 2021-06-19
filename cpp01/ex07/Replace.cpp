846caefb26aaf267fa94918f0b6d6fc8109183e1
rnakai <rnakai@student.42tokyo.jp>
2021-06-19 13:32:50 +0900

    guacamolleでもコンパイルできるように調整

--- cpp01/ex07/Replace.cpp
+++ cpp01/ex07/Replace.cpp
@@ -5,10 +5,22 @@
 
 void Replace::ft_sed(std::string file_name, std::string replaced, std::string dest)
 {
-	std::ifstream ifs(get_inputstream(file_name));
-	std::ofstream ofs(get_outputstream(file_name));
+	std::ifstream ifs;
+	std::ofstream ofs;
 	std::string buf;
+	std::string replaced_file;
 
+	ifs.open(file_name);
+	if (!ifs) {
+		std::cout << "Error: Failed to open the file: " << file_name << std::endl;
+		exit (3);
+	}
+	replaced_file = file_name + ".replace";
+	ofs.open(replaced_file);
+	if (!ofs) {
+		std::cout << "Error: Failed to create the file: " << file_name << std::endl;
+		exit(2);
+	}
 	while (true) {
 		std::getline(ifs, buf);
 		buf = replace_str(buf, replaced, dest);
