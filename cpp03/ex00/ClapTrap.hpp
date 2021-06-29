1d7aa30edd6cade096fed7657c0fcc3a26743680
rnakai <rnakai@student.42tokyo.jp>
2021-06-29 17:18:49 +0900

    ex300完

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,23 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+
+class ClapTrap
+{
+public:
+    ClapTrap(std::string name);
+    ~ClapTrap();
+
+    void attack(std::string const &target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+    void showHp(void);
+private:
+    std::string name;
+    int hitpoints;
+    int energy_points;
+    int attack_damage;
+};
+
+#endif
--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -1,28 +1,12 @@
-#include "FragTrap.hpp"
+#include "ClapTrap.hpp"
 
 int	main(void)
 {
-    srand(time(0));
-    FragTrap	frag("lelouch");
+    ClapTrap	mayuri("mayuri");
 
-    frag.meleeAttack("suzaku");
-    frag.rangedAttack("suzaku");
-
-    frag.takeDamage(30);
-    frag.takeDamage(30);
-    frag.takeDamage(30);
-    frag.takeDamage(30);
-    frag.show_hp();
-    frag.takeDamage(30);
-    frag.show_hp();
-    frag.beRepaired(60);
-    frag.takeDamage(30);
-
-    frag.show_hp();
-
-    frag.vaulthunter_dot_exe("Jack");
-    frag.vaulthunter_dot_exe("Jack");
-    frag.vaulthunter_dot_exe("Jack");
-    frag.vaulthunter_dot_exe("Jack");
-    frag.vaulthunter_dot_exe("Jack");
+    mayuri.attack("Okarin");
+    mayuri.takeDamage(5);
+    mayuri.beRepaired(10);
+    mayuri.takeDamage(100);
+    return (0);
 }
