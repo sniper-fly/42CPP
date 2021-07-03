f133fa4cd917400a12ef0a37158e532c2396bbbb
rnakai <rnakai@student.42tokyo.jp>
2021-07-03 15:47:25 +0900

    diamondTrapの雛形

---
+++ cpp03/ex03/DiamondTrap.cpp
@@ -0,0 +1,25 @@
+#include "DiamondTrap.hpp"
+#include "FragTrap.hpp"
+#include "ScavTrap.hpp"
+#include <string>
+#include <iostream>
+
+DiamondTrap::DiamondTrap(std::string name):
+    ClapTrap(name), ScavTrap(name), FragTrap(name)
+{
+    ClapTrap::name = this->name + "_clap_name";
+    this->hitpoints = FragTrap::hitpoints;
+    this->energy_points = ScavTrap::energy_points;
+    this->attack_damage = FragTrap::attack_damage;
+}
+
+DiamondTrap::~DiamondTrap()
+{
+    std::cout << "This must be the choice of steins;gate!!" << std::endl;
+}
+
+void DiamondTrap::whoAmI()
+{
+    std::cout << "This instance name is " << this->name
+    << ", ClapTrap name is " << ClapTrap::name << std::endl;
+}
---
+++ cpp03/ex03/DiamondTrap.hpp
@@ -0,0 +1,17 @@
+#ifndef DIAMONDTRAP_HPP
+#define DIAMONDTRAP_HPP
+
+#include "FragTrap.hpp"
+#include "ScavTrap.hpp"
+#include <string>
+
+class DiamondTrap : public FragTrap, public ScavTrap
+{
+public:
+    DiamondTrap(std::string name);
+    virtual ~DiamondTrap();
+
+    void whoAmI();
+};
+
+#endif
