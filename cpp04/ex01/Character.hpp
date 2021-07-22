ec918f9d7172977693c6d6c5e1d433be85a53033
rnakai <rnakai@student.42tokyo.jp>
2021-07-22 14:00:15 +0900

    Character, attack not tested yet

--- cpp04/ex01/Character.hpp
+++ cpp04/ex01/Character.hpp
@@ -2,25 +2,33 @@
 #define CHARACTER_HPP
 
 #include <string>
-#include <AWeapon.hpp>
-#include <Enemy.hpp>
+#include "AWeapon.hpp"
+#include "Enemy.hpp"
 
+//TODO: attackに関しては武器が無いとテストできない
 class Character
 {
 private:
     Character();
 public:
-    Character(std::string& const name);
-    Character(Character& const other);
+    Character(std::string const & name);
+    Character(Character& other);
     virtual ~Character();
-    Character& operator=(Character& const other);
+    Character& operator=(Character& other);
 
     void        recoverAP();
     void        equip(AWeapon*);
     void        attack(Enemy*);
     std::string getName() const;
+    int         getAP() const;
+    AWeapon*    getWeapon() const;
+
+private:
+    std::string     name;
+    int             action_point;
+    AWeapon*        weapon;
 };
 
-
+std::ostream &operator<<(std::ostream &out, Character const & other);
 
 #endif
---
+++ cpp04/ex01/main.cpp
@@ -0,0 +1,43 @@
+#include "AWeapon.hpp"
+#include "Character.hpp"
+#include "Enemy.hpp"
+
+int main()
+{
+    Character me("hoge");
+    Enemy en(100, "type");
+
+    me.recoverAP();
+    std::cout << me << std::endl;
+    me.attack(&en);
+
+    Character me2(me);
+    me2.getName();
+    me2.recoverAP();
+
+    std::cout << me;
+    std::cout << me2;
+}
+
+/*
+int main()
+{
+    Character* me = new Character("me");
+    std::cout << *me;
+    Enemy* b = new RadScorpion();
+    AWeapon* pr = new PlasmaRifle();
+    AWeapon* pf = new PowerFist();
+    me->equip(pr);
+    std::cout << *me;
+    me->equip(pf);
+    me->attack(b);
+    std::cout << *me;
+    me->equip(pr);
+    std::cout << *me;
+    me->attack(b);
+    std::cout << *me;
+    me->attack(b);
+    std::cout << *me;
+    return 0;
+}
+*/
