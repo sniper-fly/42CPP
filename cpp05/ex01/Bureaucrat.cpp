f8d23f696f69144c108ee03a58d84278d993f98d
rnakai <rnakai@student.42tokyo.jp>
2021-09-17 09:53:41 +0900

    ファイルコピー

---
+++ cpp05/ex01/Bureaucrat.cpp
@@ -0,0 +1,68 @@
+#include "Bureaucrat.hpp"
+#include <iostream>
+#include <string>
+
+// 今回使用しない
+Bureaucrat::Bureaucrat() { }
+Bureaucrat &Bureaucrat::operator=(Bureaucrat const &other)
+{
+    (void)other;
+    return *this;
+}
+Bureaucrat::Bureaucrat(Bureaucrat const &other) { (void)other; }
+//
+
+void                    Bureaucrat::assign_grade(int grade)
+{
+    if (grade < 1) {
+        this->grade = 1;
+        throw GradeTooHighException();
+    } else if (grade > 150) {
+        this->grade = 150;
+        throw GradeTooLowException();
+    } else {
+        this->grade = grade;
+    }
+}
+
+Bureaucrat::Bureaucrat(int grade, std::string name): name(name)
+{
+    assign_grade(grade);
+}
+
+Bureaucrat::~Bureaucrat() { }
+
+std::string const &     Bureaucrat::getName() const { return name; }
+int const &             Bureaucrat::getGrade() const { return grade; }
+
+void                    Bureaucrat::incrementGrade() { assign_grade(grade - 1); }
+
+void                    Bureaucrat::decrementGrade() { assign_grade(grade + 1); }
+
+Bureaucrat::GradeTooHighException::GradeTooHighException() {}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return "Error: GradeTooHighException: grade is too high";
+}
+
+Bureaucrat::GradeTooLowException::GradeTooLowException() {}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return "Error: GradeTooLowException: grade is too low";
+}
+
+std::ostream& operator<<(std::ostream &out, const Bureaucrat &other)
+{
+    out << "<" << other.getName() << ">, bureaucrat grade <" << other.getGrade()
+    << ">.";
+
+    return out;
+}
+
+/* TODO
+各種コンストラクタなど
+例外クラス実装
+インクリメントなど
+*/
