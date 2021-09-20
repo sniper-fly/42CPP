d88ccc0ddc8213752fb327fb88f9999dbf5c99d0
rnakai <rnakai@student.42tokyo.jp>
2021-09-21 00:18:26 +0900

    mainのテスト追加

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    // test exception
+    /* *******previous test*******
     for (int i = -2; i < 155; ++i) {
         try {
             Bureaucrat test(i, "test");
@@ -11,11 +11,41 @@ int main(void)
             std::cout << e.what() << ": " << i << std::endl;
         }
     }
-
     // test if << operator works
     std::cout << "--------------------------------" << std::endl;
     for (int i = 1; i < 10; ++i) {
         Bureaucrat test(i, "test2");
         std::cout << test << std::endl;
     }
+    */
+
+    for (int i = 0; i < 155; ++i) {
+        try {
+            Form test("test", i, i - 2);
+            std::cout << test << std::endl;
+        } catch (std::exception & e) {
+            std::cout
+            << e.what() << ", "
+            << "i: " << i << ", "
+            << "i - 2: " << i - 2
+            << std::endl;
+        }
+    }
+
+    std::cout << "---------signForm test--------" << std::endl;
+    Form five_form("five_form", 5, 10);
+    Bureaucrat ten(10, "ten_man");
+    ten.signForm(five_form);
+
+    std::cout << "------------------------------" << std::endl;
+    Bureaucrat five(5, "five_man");
+    five.signForm(five_form);
+    std::cout << five_form << std::endl;
+
+    std::cout << "------------------------------" << std::endl;
+    Form five_form2("five_form", 5, 10);
+    Bureaucrat four(4, "five_man");
+    four.signForm(five_form2);
+    std::cout << five_form2 << std::endl;
+
 }
