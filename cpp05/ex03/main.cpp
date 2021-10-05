724075912c09803ef833f929dfce9994ebd98178
rnakai <rnakai@student.42tokyo.jp>
2021-10-05 10:42:45 +0900

    ex3テスト完成

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -11,14 +11,14 @@ int main(void)
     int tests[] = {0, 1, 5, 25, 45, 72, 137, 145, 150, 151} ;
     int len = 10;
     for (int i = 0; i < len; ++i) {
+        Form *pre = intern.makeForm("presidential pardon", "pre");
+        Form *robo = intern.makeForm("robotomy request", "robo");
+        Form *shrube = intern.makeForm("shrubbery creation", "shrube");
         try {
             std::cout << "==============================" << std::endl;
             std::cout << "test case:" << tests[i] << std::endl;
             std::cout << "==============================" << std::endl;
             Bureaucrat bur(tests[i], "bur");
-            Form *pre = intern.makeForm("presidential pardon", "pre");
-            Form *robo = intern.makeForm("robotomy request", "robo");
-            Form *shrube = intern.makeForm("shrubbery creation", "shrube");
 
             bur.signForm(*shrube);
             shrube->execute(bur);
@@ -29,14 +29,13 @@ int main(void)
             bur.signForm(*pre);
             pre->execute(bur);
             std::cout << std::endl;
-
-            delete pre;
-            delete robo;
-            delete shrube;
         } catch (std::exception &e) {
             std::cout << e.what() << std::endl;
             std::cout << std::endl;
         }
+        delete pre;
+        delete robo;
+        delete shrube;
     }
 
     Intern::_delete_forms();
