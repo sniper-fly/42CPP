8516adb0c1f4a73e41e4ed54a8b8638a5f115d3a
rnakai <rnakai@student.42tokyo.jp>
2021-10-10 16:01:52 +0900

    ex02 comp

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -19,17 +19,40 @@ void identify(Base* p)
     }
 }
 
-// void identify(Base& p)
-// {
-
-// }
+void identify(Base& p)
+{
+    try {
+        (void)dynamic_cast<A&>(p);
+        std::cout << "A" << std::endl;
+        return ;
+    }
+    catch (...) {}
+    try {
+        (void)dynamic_cast<B&>(p);
+        std::cout << "B" << std::endl;
+        return ;
+    }
+    catch (...) {}
+    try {
+        (void)dynamic_cast<C&>(p);
+        std::cout << "C" << std::endl;
+        return ;
+    }
+    catch (...) {}
+}
 
 int main()
 {
     Base* instances[10];
     for (int i = 0; i < 10; ++i) {
         instances[i] = generate();
+        std::cout << "pointer: ";
         identify(instances[i]);
+
+        std::cout << "reference: ";
+        identify(*instances[i]);
+
+        std::cout << "--------------------" << std::endl;
         delete instances[i];
     }
 }
