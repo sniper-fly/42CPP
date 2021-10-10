151a2c4fddd25a3b007c22e84a7efd2b4c845f8a
rnakai <rnakai@student.42tokyo.jp>
2021-10-10 15:54:28 +0900

    implemented identify func with pointer param

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -2,21 +2,34 @@
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
+#include <iostream>
 
 Base * generate(void);
 
-// void identify(Base* p);
-// {
+void identify(Base* p)
+{
+    if (dynamic_cast<A*>(p)) {
+        std::cout << "A" << std::endl;
+    }
+    else if (dynamic_cast<B*>(p)) {
+        std::cout << "B" << std::endl;
+    }
+    else if (dynamic_cast<C*>(p)) {
+        std::cout << "C" << std::endl;
+    }
+}
 
-// }
-
-// void identify(Base& p);
+// void identify(Base& p)
 // {
 
 // }
 
 int main()
 {
-    Base* hoge = generate();
-    delete hoge;
+    Base* instances[10];
+    for (int i = 0; i < 10; ++i) {
+        instances[i] = generate();
+        identify(instances[i]);
+        delete instances[i];
+    }
 }
