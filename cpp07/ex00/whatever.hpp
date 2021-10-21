20197cebec0b93ef3c804e9a56a1215965bca99b
rnakai <rnakai@student.42tokyo.jp>
2021-10-21 23:21:00 +0900

    関数テンプレートex00の完成

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -1,5 +1,25 @@
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
+template <typename TYPE>
+void swap(TYPE &a, TYPE &b)
+{
+    TYPE tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename TYPE>
+TYPE min(TYPE a, TYPE b)
+{
+    return (a >= b) ? b : a;
+}
+
+template <typename TYPE>
+TYPE max(TYPE a, TYPE b)
+{
+    return (a <= b) ? b : a;
+}
 
 #endif
