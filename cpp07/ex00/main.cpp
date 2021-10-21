fbd038ada55ef0ea79d91fe0abd228e6d0c0734f
rnakai <rnakai@student.42tokyo.jp>
2021-10-21 19:11:55 +0900

    コード整形

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -5,23 +5,19 @@ int main( void )
 {
     int a = 2;
     int b = 3;
-    ::swap( a, b
-    std::cout <<
-    std::cout <<
-    std::cout <<
-    );
-    "a = " << a << ", b = " << b << std::endl;
-    "min( a, b ) = " << ::min( a, b ) << std::endl;
-    "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
     std::string c = "chaine1";
     std::string d = "chaine2";
-    ::swap(c,
-    std::cout
-    std::cout
-    std::cout
-    d);
-    << "c = " << c << ", d = " << d << std::endl;
-    << "min( c, d ) = " << ::min( c, d ) << std::endl;
-    << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    ::swap(c, d);
+
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
     return 0;
 }
