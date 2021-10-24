7b6f33ad77fb15ea04be95e1454f7bd566df078e
rnakai <rnakai@student.42tokyo.jp>
2021-10-24 11:28:27 +0900

    基本機能実装ex02

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,11 +1,13 @@
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 #include <cstddef>
+#include <exception>
 
 template <typename T>
 class Array
 {
 private:
+    T*                array;
     unsigned int      length;
 public:
     Array();
@@ -19,33 +21,56 @@ public:
 };
 
 template <typename T>
-Array<T>::Array() { }
+Array<T>::Array()
+{
+    length = 0;
+    array = new T[0];
+}
 
 template <typename T>
-Array<T>::Array(unsigned int n) { }
+Array<T>::Array(unsigned int n)
+{
+    length = n;
+    array = new T[n];
+}
 
 template <typename T>
-Array<T>::~Array() { }
+Array<T>::~Array()
+{
+    delete[] array;
+    array = NULL;
+}
 
 template <typename T>
-Array<T>::Array(Array<T> const &other) { }
+Array<T>::Array(Array<T> const &other)
+{
+    *this = other;
+}
 
 template <typename T>
 Array<T>&          Array<T>::operator=(Array<T> const &other)
 {
-    ;
+    delete[] array;
+    length = other.length;
+    array = new T[length];
+    for (int i = 0; i < other.length; ++i) {
+        array[i] = (other.array)[i];
+    }
 }
 
 template <typename T>
 T&              Array<T>::operator[](unsigned int idx)
 {
-    ;
+    if (idx >= length) {
+        throw std::exception();
+    }
+    return array[idx];
 }
 
 template <typename T>
 unsigned int    Array<T>::size(void) const
 {
-    ;
+    return length;
 }
 
 #endif
