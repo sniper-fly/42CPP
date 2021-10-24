a1fb5f08b4193db02f1f959fb7e44f7fd85d5be2
rnakai <rnakai@student.42tokyo.jp>
2021-10-24 14:08:19 +0900

    全テスト実施

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,5 +1,6 @@
 #include "Array.hpp"
 #include <iostream>
+#include <exception>
 
 int main()
 {
@@ -9,16 +10,80 @@ int main()
     Array<float> arr2(10);
     for (int i = 0; i < 10; ++i) {
         arr2[i] = i;
+        if (arr2[i] != i) {
+            std::cerr << "float error" << std::endl;
+            return 1;
+        }
     }
     std::cout << "===========================" << std::endl;
     for (int i = 0; i < 10; ++i) {
         std::cout << arr2[i] << std::endl;
     }
+    std::cout << "===========================" << std::endl;
 
     // 範囲外で例外を出すか
+    try {
+        arr2[9] = 10;
+        std::cout << 9 << std::endl;
+        arr2[10] = 10;
+        std::cout << 10 << std::endl;
+    } catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
+    try {
+        arr1[0] = 10;
+        std::cout << 0 << std::endl;
+    } catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
+
     // string型で動作するか
-    // コピーコンストラクタや=オペレータはディープコピーなのか メモリリークはないか
-    
+    std::cout << "===========================" << std::endl;
+    const std::string strs[5] = {
+        "apple",
+        "banana",
+        "candy",
+        "does",
+        "egg"
+    };
+    Array<std::string> arr_str(5);
+    for (unsigned int i = 0; i < arr_str.size(); ++i) {
+        arr_str[i] = strs[i];
+        if (arr_str[i] != strs[i]) {
+            std::cerr << "string array failure" << std::endl;
+            return 1;
+        }
+        std::cout << arr_str[i] << std::endl;
+    }
+
+    std::cout << "===========================" << std::endl;
+    //memory leak check
+    Array<std::string> clone2(2109);
+    clone2 = arr_str;
+
+    // コピーコンストラクタや=オペレータはディープコピーなのか
+    std::cout << "===========================" << std::endl;
+    Array<std::string> clone;
+    clone = arr_str;
+    for (unsigned int i = 0; i < clone.size(); ++i) {
+        clone[i] += "hoge"; // append "hoge" at the end of every elem
+        if (clone[i] == arr_str[i]) {
+            std::cout << "assignment: deep copy failure" << std::endl;
+            return 1;
+        }
+        std::cout << clone[i] << std::endl;
+    }
+    std::cout << "===========================" << std::endl;
+    Array<std::string> clone3(arr_str);
+    for (unsigned int i = 0; i < clone3.size(); ++i) {
+        clone3[i] += "hoge";
+        if (clone3[i] == arr_str[i]) {
+            std::cerr << "copy constructor: deep copy failure" << std::endl;
+            return 1;
+        }
+        std::cout << clone3[i] << std::endl;
+        // std::cout << clone[i] << std::endl;
+    }
 }
 
 /*
