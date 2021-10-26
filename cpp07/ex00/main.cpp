4065524db2578b0300ed079a076376368ec0058f
rnakai <rnakai@student.42tokyo.jp>
2021-10-26 11:55:56 +0900

    mainにテスト組み込み

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "whatever.hpp"
 
-/*
+
 int main( void )
 {
     int a = 2;
@@ -22,8 +22,9 @@ int main( void )
 
     return 0;
 }
-*/
 
+
+/*
 int main()
 {
     //別の型でも同じ関数でスワップできるか
@@ -64,3 +65,42 @@ int main()
         std::cout << "didn't return second one" << std::endl;
     }
 }
+*/
+
+/*
+class Awesome {
+public:
+    Awesome()
+    {
+        _n = 0;
+    }
+    Awesome( int n ) : _n( n ) {}
+    bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+    bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+    bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+    bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+    bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+    bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+
+    int get_n() { return _n; }
+    Awesome& operator=(Awesome &other)
+    {
+        _n = other._n;
+        return *this;
+    }
+
+private:
+    int _n;
+};
+
+int main()
+{
+    Awesome a(3);
+    Awesome b(2);
+    swap(a, b);
+    std::cout << "a = " << a.get_n()  << std::endl;
+    std::cout << "b = " << b.get_n()  << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ).get_n() << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ).get_n() << std::endl;
+}
+*/
