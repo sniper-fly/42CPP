6778903400e9f947cd57c3ed4f62ee9423461f05
rnakai <rnakai@student.42tokyo.jp>
2021-10-28 14:45:55 +0900

    hpp雛形作成

--- cpp08/ex01/span.hpp
+++ cpp08/ex01/span.hpp
@@ -1,15 +1,31 @@
 #ifndef SPAN_HPP
 # define SPAN_HPP
+#include <vector>
 
 class Span
 {
 private:
+    std::vector<int>    numbers;
+    unsigned int        max;
+    Span();
 
 public:
-    Span();
+    Span(unsigned int N);
     virtual ~Span();
     Span(Span const &other);
     Span &operator=(Span const &other);
+
+    void            addNumber(int num);
+    template <typename InputIterator>
+    void            addNumber(InputIterator begin, InputIterator end)
+    {
+        //エラーチェック
+
+        numbers.assign(begin, end);
+    }
+
+    unsigned int    shortestSpan();
+    unsigned int    longestSpan(); //intの最小値と最大値の差も入る？
 };
 
 #endif
