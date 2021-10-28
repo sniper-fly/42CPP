45588479aac5d67cf7944b225b0879fb8d3786da
rnakai <rnakai@student.42tokyo.jp>
2021-10-28 23:48:51 +0900

    makeのフラグでコンパイルするmainを変更できるようにした

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -6,6 +6,22 @@
 #include <limits>
 #include <exception>
 
+#ifndef my_test
+int main()
+{
+    Span sp = Span(5);
+    sp.addNumber(5);
+    sp.addNumber(3);
+    sp.addNumber(17);
+    sp.addNumber(9);
+    sp.addNumber(11);
+    std::cout << sp.shortestSpan() << std::endl;
+    std::cout << sp.longestSpan() << std::endl;
+}
+#endif
+
+#ifdef my_test
+
 void print_test_title(const std::string& title)
 {
     std::cout << std::endl;
@@ -140,3 +156,5 @@ int main()
         }
     }
 }
+
+#endif
