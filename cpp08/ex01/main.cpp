5937df3607615d87608920caaa5f8e16fb279a49
rnakai <rnakai@student.42tokyo.jp>
2021-10-28 18:09:29 +0900

    全体テストが完成

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -9,53 +9,86 @@
 int main()
 {
     srand(time(0));
-    Span sp = Span(10002);
-    std::vector<int> vec;
-    for (int i = 0; i < 10000; ++i) {
-        vec.push_back(rand() % 100000);
+    // normal test
+    std::cout << "=================" << std::endl;
+    std::cout << "normal test" << std::endl;
+    std::cout << "=================" << std::endl;
+    {
+        Span sp(20);
+        for (int i = 0; i < 20; ++i) {
+            sp.addNumber(i * 2);
+        }
+        std::cout << sp.shortestSpan() << std::endl;
+        std::cout << sp.longestSpan() << std::endl;
     }
-    
-    sp.addNumber(vec.begin(), vec.end());
-    std::cout << sp.shortestSpan() << std::endl;
-    std::cout << sp.longestSpan() << std::endl;
+    std::cout << std::endl;
 
-    sp.addNumber(std::numeric_limits<int>::min());
-    sp.addNumber(std::numeric_limits<int>::max());
-    std::cout << sp.shortestSpan() << std::endl;
-    std::cout << sp.longestSpan() << std::endl;
+    // random numbers test
+    std::cout << "=================" << std::endl;
+    std::cout << "random numbers test" << std::endl;
+    std::cout << "=================" << std::endl;
+    {
+        Span sp = Span(10002);
+        std::vector<int> vec;
+        for (int i = 0; i < 10000; ++i) {
+            vec.push_back(rand() % 100000);
+        }
+        sp.addNumber(vec.begin(), vec.end());
+        std::cout << sp.shortestSpan() << std::endl;
+        std::cout << sp.longestSpan() << std::endl;
 
-    // try {
-    //     sp.addNumber(1);
-    // } catch (std::exception &e) {
-    //     std::cout << e.what() << std::endl;
-    // }
+        // int max - int min
+        sp.addNumber(std::numeric_limits<int>::min());
+        sp.addNumber(std::numeric_limits<int>::max());
+        std::cout << sp.shortestSpan() << std::endl;
+        std::cout << sp.longestSpan() << std::endl;
+    }
+    std::cout << std::endl;
 
-    // //何も値が入っていない状態でのshortest, longestSpan
-    // Span sp2 = Span(10000);
-    // try {
-    //     std::cout << sp.shortestSpan() << std::endl;
-    // } catch (std::exception &e) {
-    //     std::cout << e.what() << std::endl;
-    // }
+    std::cout << "=================" << std::endl;
+    std::cout << "empty number test" << std::endl;
+    std::cout << "=================" << std::endl;
+    {
+        Span sp(0);
+        try {
+            sp.addNumber(1);
+        } catch (std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+    }
+    std::cout << std::endl;
 
-    // try {
-    //     std::cout << sp.longestSpan() << std::endl;
-    // } catch (std::exception &e) {
-    //     std::cout << e.what() << std::endl;
-    // }
+    std::cout << "=================" << std::endl;
+    std::cout << "span test with not enough numbers" << std::endl;
+    std::cout << "=================" << std::endl;
+    {
+        //何も値が入っていない状態でのshortest, longestSpan
+        Span sp = Span(10000);
+        try {
+            std::cout << sp.shortestSpan() << std::endl;
+        } catch (std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
 
-    // //値が１しか入っていない状態でのshortest, longestSpan
-    // sp2.addNumber(1);
-    // try {
-    //     std::cout << sp.shortestSpan() << std::endl;
-    // } catch (std::exception &e) {
-    //     std::cout << e.what() << std::endl;
-    // }
+        try {
+            std::cout << sp.longestSpan() << std::endl;
+        } catch (std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
 
-    // try {
-    //     std::cout << sp.longestSpan() << std::endl;
-    // } catch (std::exception &e) {
-    //     std::cout << e.what() << std::endl;
-    // }
+        //値が１しか入っていない状態でのshortest, longestSpan
+        sp.addNumber(1);
+        try {
+            std::cout << sp.shortestSpan() << std::endl;
+        } catch (std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
 
+        try {
+            std::cout << sp.longestSpan() << std::endl;
+        } catch (std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+    }
+    std::cout << std::endl;
 }
